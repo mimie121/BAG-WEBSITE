c72190eba6049fc1842c895dc351e38dccd61efa
mimie121 <talk2mimie.reply@gmail.com>
2024-06-21 09:36:36 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,55 @@
+// window.onscroll = function() {myFunction()};
+
+// var navbar = document.getElementById("navbar");
+// var sticky = navbar.offsetTop;
+
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
+
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+
+var bar = document.getElementById('bar');
+
+var nav = document.getElementById('navbar');
+
+
+if (bar) {
+  bar.addEventListener('click', () => {
+    nav.classList.add('active1');
+  
+})
+}
+
+// if (close) {
+//   close.addEventListener('click', () => {
+//       nav.classList.remove('active');
+
+// })
+// }
+ 
